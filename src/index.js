4acbb40f764ec659f2366682e44dc5be2ca98855
banba81816900 <77661739+banba81816900@users.noreply.github.com>
2021-01-19 17:52:01 +0900

    変数宣言　var・let・const

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import "./styles.css";
+/**
+ * const let の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
+//var変数は上書き可能
+val1 = "var変数を上書き";
+console.log(val1);
+
+//var変数の再宣言
+var val1 = "var変数を再宣言";
+console.log(val1);
+
+//let変数
+let val2 = "let変数";
+console.log(val2);
+
+//let変数は上書き可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+//let変数は再宣言不可
+//let val2 = "let変数を再宣言"→できない
+
+const val3 = "val3変数";
+console.log(val3);
+
+//constは上書き・再宣言不可
